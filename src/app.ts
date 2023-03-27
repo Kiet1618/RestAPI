59fd73dcc6d62b68bbe768fd733fa687e8a07345
Kiet1618 <ngokiet3207@gmail.com>
2023-03-28 02:23:15 +0700

    update env

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,60 @@
-import { Route } from 'core/interface';
+import { Route } from '@core/interface';
 import express from 'express';
+import mongoose from 'mongoose';
+import hpp from 'hpp';
+import morgan from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
+import { Logger } from '@core/utils/index';
 
 class App {
     public app: express.Application;
     public port: string | number;
+    public production: boolean;
 
     constructor(routes: Route[]) {
         this.app = express();
         this.port = process.env.PORT || 5000;
+        this.production = process.env.NODE_ENV == 'production' ? true : false;;
 
-        this.initializeRoutes(routes)
+        this.connectToDatabase();
+        this.initializeRoutes(routes);
+        this.initializeMiddleware();
     }
     public listen() {
         this.app.listen(this.port, () => {
-            console.log('listening on port ' + this.port);
+            Logger.info('listening on port ' + this.port);
         });
     }
     private initializeRoutes(routers: Route[]) {
         routers.forEach(route => {
             this.app.use('/', route.router)
         });
+    }
+    private connectToDatabase() {
+        const connectString = process.env.MONGODB_URI;
+        if (!connectString) {
+            return;
+        }
+        try {
+            mongoose.connect(connectString);
+            Logger.info('Connection successful');
+        }
+        catch (err) {
+            Logger.error(err);
+        }
+    }
+    private initializeMiddleware() {
+        if (this.production) {
+            this.app.use(hpp());
+            this.app.use(helmet());
+            this.app.use(morgan('combined'));
+            this.app.use(cors({ origin: 'your-domain.com', credentials: true }));
+        }
+        else {
+            this.app.use(morgan('dev'));
+            this.app.use(cors({ origin: true, credentials: true }));
+        }
     }
 }
 export default App;
